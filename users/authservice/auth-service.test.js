fecf63fadb61ad11041049786809a26bdfcd6d46
Diego <38155922+Diegomcha@users.noreply.github.com>
2025-03-03 12:57:19 +0100

    style(auth): :art: run Prettier

--- users/authservice/auth-service.test.js
+++ users/authservice/auth-service.test.js
@@ -1,18 +1,18 @@
-const request = require('supertest');
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const bcrypt = require('bcrypt');
-const User = require('./auth-model');
+const request = require("supertest");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const bcrypt = require("bcrypt");
+const User = require("./auth-model");
 
 let mongoServer;
 let app;
 
 //test user
 const user = {
-  username: 'testuser',
-  password: 'testpassword',
+  username: "testuser",
+  password: "testpassword",
 };
 
-async function addUser(user){
+async function addUser(user) {
   const hashedPassword = await bcrypt.hash(user.password, 10);
   const newUser = new User({
     username: user.username,
@@ -26,7 +26,7 @@ beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
   const mongoUri = mongoServer.getUri();
   process.env.MONGODB_URI = mongoUri;
-  app = require('./auth-service'); 
+  app = require("./auth-service");
   //Load database with initial conditions
   await addUser(user);
 });
@@ -36,10 +36,10 @@ afterAll(async () => {
   await mongoServer.stop();
 });
 
-describe('Auth Service', () => {
-  it('Should perform a login operation /login', async () => {
-    const response = await request(app).post('/login').send(user);
+describe("Auth Service", () => {
+  it("Should perform a login operation /login", async () => {
+    const response = await request(app).post("/login").send(user);
     expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('username', 'testuser');
+    expect(response.body).toHaveProperty("username", "testuser");
   });
 });
