4f7b1b9539b6e16a91896c3008ff010ed1035ff6
Diego <38155922+Diegomcha@users.noreply.github.com>
2025-03-18 23:22:08 +0100

    feat(gateway): :sparkles: revamped gateway service
    Signed-off-by: Diego <38155922+Diegomcha@users.noreply.github.com>

---
+++ gatewayservice/src/auth.js
@@ -0,0 +1,45 @@
+// @ts-check
+
+const { default: axios } = require("axios");
+const config = require("./config");
+const { STATUS_CODES } = require("http");
+
+/**
+ * @param {import("express").Application} app
+ */
+module.exports = (app) =>
+  app.use(config.auth.paths, async (req, res, next) => {
+    const [type, token] = req.headers.authorization?.split(/\s+/) ?? [];
+
+    // Perform verification
+    try {
+      // Verify token exists and is 'Bearer'
+      if (type?.toLowerCase() !== "bearer" || token == null || token === "")
+        throw Error("Invalid token");
+
+      const verification = await axios.post(
+        config.auth.url,
+        {
+          token,
+        },
+        { timeout: 1_000 }
+      );
+
+      // Attach logged-in username to headers
+      req.headers.authorization = `Basic ${Buffer.from(
+        verification.data.username
+      ).toString("base64")}`;
+
+      next();
+    } catch (err) {
+      if (
+        err.message === "Invalid token" ||
+        err.status?.toString().startsWith("4")
+      )
+        res
+          .header("WWW-Authenticate", "Bearer")
+          .status(401)
+          .json({ success: false, message: STATUS_CODES[401] });
+      else next(err);
+    }
+  });
