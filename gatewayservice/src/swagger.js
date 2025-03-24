db67c26292b37a14522bc28366f412670f5a02c3
Diego <38155922+Diegomcha@users.noreply.github.com>
2025-03-24 12:56:45 +0100

    refactor(gateway): :recycle: refactored swagger

--- gatewayservice/src/swagger.js
+++ gatewayservice/src/swagger.js
@@ -9,15 +9,18 @@ const openapiPath = "./openapi.yaml";
  * @param {import("express").Application} app
  */
 module.exports = (app) => {
-  if (fs.existsSync(openapiPath)) {
-    try {
-      const file = fs.readFileSync(openapiPath, "utf8");
-      const swaggerDocument = YAML.parse(file);
-      app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-    } catch (err) {
-      console.error("Error parsing OpenAPI file:", err.message);
-    }
-  } else {
-    console.log("Not configuring OpenAPI. Configuration file not present.");
+  if (!fs.existsSync(openapiPath))
+    return console.warn(
+      "Not configuring OpenAPI. Configuration file not present."
+    );
+
+  try {
+    app.use(
+      "/api-doc",
+      swaggerUi.serve,
+      swaggerUi.setup(YAML.parse(fs.readFileSync(openapiPath, "utf8")))
+    );
+  } catch (err) {
+    console.error("Error parsing OpenAPI file:", err.message);
   }
 };
