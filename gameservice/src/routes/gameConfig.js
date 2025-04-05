8428cf8402a433f20f24e9a8883cb44df86cc854
UO288992 <UO288992@uniovi.es>
2025-04-05 21:09:11 +0200

    fix(game): :bug: Added missing game service port and additions to docker-compose

--- gameservice/src/routes/gameConfig.js
+++ gameservice/src/routes/gameConfig.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
 
     app.get('/game/config', async (req, res) => {
         const gameConfig = {
+            port: process.env.PORT || 8001,
             time: config.time,
             rounds: config.rounds,
             hints: config.hints
