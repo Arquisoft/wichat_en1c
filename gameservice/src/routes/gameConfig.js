4136607a4f0d3d7c649585653da21ffc1f836e56
UO288992 <UO288992@uniovi.es>
2025-04-26 10:14:40 +0200

    fix(game): :bug: Remove port from /game/config endpoint.

--- gameservice/src/routes/gameConfig.js
+++ gameservice/src/routes/gameConfig.js
@@ -4,12 +4,10 @@ module.exports = (app) => {
 
     app.get('/game/config', async (req, res) => {
         const gameConfig = {
-            port: config.port,
             time: config.time,
             rounds: config.rounds,
             hints: config.hints
         }
         res.json(gameConfig);
     });
-
 };
